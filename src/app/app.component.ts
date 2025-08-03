d6f6006fd7ab9e37533b6d692d9e9e55987b9746
Bruno <b.rezzio.93@gmail.com>
2025-08-03 15:08:31 -0400

    se creó componente game-options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameOptionsComponent } from './modules/game-options/game-options.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    GameOptionsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/modules/game-options/game-options.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-game-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-options.component.html',
+  styleUrl: './game-options.component.css'
+})
+
+export class GameOptionsComponent implements OnInit {
+  playersQty: number = 1;
+  villains: any;
+  villain: any;
+
+  constructor(
+    private dataService: DataService,
+  ) { }
+
+  ngOnInit(): void {
+    this.initVillains();
+    console.log("Game Options Component Initialized");
+  }
+
+
+  initVillains() {
+    this.dataService.getEncounterCardsData().subscribe(
+      {
+        next: (response: any) => {
+          let encounterPacks: any[] = [];
+
+          response.forEach((card: any) => {
+            if (card.type_code == 'villain' || card.type_code == 'main_scheme') {
+              if (encounterPacks[card.card_set_code] == undefined) encounterPacks[card.card_set_code] = [];
+              encounterPacks[card.card_set_code].card_set_code = card.card_set_code;
+              encounterPacks[card.card_set_code].text = card.card_set_name;
+
+              if (card.type_code == 'villain') {
+                if (encounterPacks[card.card_set_code].villain_phases == undefined) encounterPacks[card.card_set_code].villain_phases = [];
+                encounterPacks[card.card_set_code].villain_phases.push(card)
+              }
+              if (card.type_code == 'main_scheme') {
+                if (encounterPacks[card.card_set_code].main_scheme == undefined) encounterPacks[card.card_set_code].main_scheme = [];
+                encounterPacks[card.card_set_code].main_scheme.push(card)
+              }
+            }
+
+          });
+
+
+          let mainSets: any[] = [];
+          let modularSets: any[] = [];
+          for (const key in encounterPacks) {
+            const pack = encounterPacks[key];
+            if (pack.main_scheme != undefined) {
+              if (pack.villain_phases) {
+                pack.villain_phases.forEach((villain_phase: any) => {
+                  villain_phase.card_text = villain_phase.text;
+                  villain_phase.text = villain_phase.name + ' ' + villain_phase.stage;
+                });
+              }
+              mainSets.push(pack);
+            }
+            else {
+              if (!pack.card_set_code.includes("_nemesis"))
+                modularSets.push(pack);
+            }
+          }
+
+          this.villains = mainSets;
+        },
+        error: (error: any) => {
+          console.log("Error getting encounter cards data: ", error);
+        },
+      }
+    );
+  }
+
+  onPlayersQtyChange(value: number) {
+    this.playersQty = value;
+    console.log("Players Quantity Changed: ", this.playersQty);
+  }
+
+  onEncounterSelected(value: string) {
+    this.villain = this.villains.find((v: any) => v.card_set_code === value);
+    console.log("Selected Villain: ", this.villain);
+  }
+
+  onStartGame() {
+    console.log("Game Started with Players Quantity: ", this.playersQty);
+    // Logic to start the game can be added here
+  }
+}
