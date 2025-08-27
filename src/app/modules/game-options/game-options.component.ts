9ba62ff6dd63e565253e4d4d80b192830e1f425d
Bruno <b.rezzio.93@gmail.com>
2025-08-27 15:00:37 -0400

    se añadieron casos de initMainSchemes para encuentros que no tengan cartas en DB

--- src/app/modules/game-options/game-options.component.ts
+++ src/app/modules/game-options/game-options.component.ts
@@ -19,6 +19,7 @@ export class GameOptionsComponent implements OnInit {
   playersQty: number = 2;
   villains: any;
   villain: any;
+  rawEncounterCards: any;
 
   constructor(
     private dataService: DataService,
@@ -33,6 +34,7 @@ export class GameOptionsComponent implements OnInit {
     this.dataService.getEncounterCardsData().subscribe(
       {
         next: (response: any) => {
+          this.rawEncounterCards = response;
           let encounterPacks: any[] = [];
 
           response = response.sort((a: any, b: any) => {
@@ -87,6 +89,19 @@ export class GameOptionsComponent implements OnInit {
   }
 
   initMainSchemes(card: any) {
+    if (card.imagesrc != undefined) card.imagesrc = 'https://es.marvelcdb.com' + card.imagesrc;
+
+    if (card.threat != undefined) {
+      if (card.threat != undefined) card.threat = card.threat * this.playersQty;
+      else card.threat = "-";
+    }
+
+    if (card.base_threat != undefined) card.base_threat = card.base_threat * this.playersQty;
+    else card.base_threat = "-";
+
+    if (card.escalation_threat != undefined) card.escalation_threat = card.escalation_threat * this.playersQty;
+    else card.escalation_threat = "-";
+
     switch (card.card_set_code) {
       case "rhino":
         if (card.code == "01097") return true;
@@ -131,7 +146,10 @@ export class GameOptionsComponent implements OnInit {
         if (card.code == "07001") return true;
         else return false;
       case "kang":
-        if (card.code == "11007") return true;
+        if (card.code == "11007") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2020/09/kang-7b-kangs-arrival.png';
+          return true;
+        }
         // if (card.code == "11010b") return true;
         // if (card.code == "11011b") return true;
         // if (card.code == "11012b") return true;
@@ -158,22 +176,46 @@ export class GameOptionsComponent implements OnInit {
         // if (card.code == "16107b") return true;
         else return false;
       case "ebony_maw":
-        if (card.code == "21074") return true;
-        // if (card.code == "21075") return true;
+        if (card.code == "21074") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2021/08/e4b.jpg';
+          return true;
+        }
+        // if (card.code == "21075") {
+        //   card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2021/08/e5b.jpg';
+        //   return true;
+        // }
         else return false;
       case "tower_defense":
-        if (card.code == "21098") return true;
-        // if (card.code == "21099") return true;
+        if (card.code == "21098") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2021/08/t7b.jpg';
+          return true;
+        }
+        if (card.code == "21099") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2021/08/t8b.jpg';
+          return true;
+        }
         else return false;
       case "thanos":
-        if (card.code == "21114") return true;
-        // if (card.code == "21115") return true;
+        if (card.code == "21114") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2021/08/t4b.jpg';
+          return true;
+        }
+        // if (card.code == "21115") {
+        //   card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2021/08/t5b.jpg';
+        //   return true;
+        // }
         else return false;
       case "hela":
-        if (card.code == "21138") return true;
+        if (card.code == "21138") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2021/08/h3b.jpg';
+          return true;
+        }
         else return false;
       case "loki":
-        if (card.code == "21165") return true;
+        if (card.code == "21165") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2021/08/l6b.jpg';
+          return true;
+        }
         else return false;
       case "the_hood":
         if (card.code == "24004b") return true;
@@ -200,16 +242,26 @@ export class GameOptionsComponent implements OnInit {
         if (card.code == "27119a") return true;
         else return false;
       case "sabretooth":
+        if (card.code == "32063b") {
+          card.threat = '-';
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2024/08/32063b.jpg';
+          return true;
+        }
         if (card.code == "32064b") return true;
         else return false;
       case "project_wideawake":
-        console.log(card.code)
-        return true
-        if (card.code == "11007") return true;
+        if (card.code == "32087b") {
+          card.threat = 5 * this.playersQty;
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2024/08/32087b.jpg';
+          return true;
+        }
         else return false;
       case "master_mold":
         if (card.code == "32112b") return true;
-        // if (card.code == "32113b") return true;
+        // if (card.code == "32113b") {
+        //   card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2024/08/32113b.jpg';
+        //   return true;
+        // }
         else return false;
       case "mansion_attack":
         if (card.code == "32126b") return true;
@@ -233,24 +285,41 @@ export class GameOptionsComponent implements OnInit {
         else return false;
       case "morlock_siege":
         if (card.code == "40077b") return true;
-        if (card.code == "40078b") return true;
+        // if (card.code == "40078b") return true;
         else return false;
       case "on_the_run":
         if (card.code == "40103b") return true;
-        if (card.code == "40104b") return true;
+        // if (card.code == "40104b") return true;
         else return false;
       case "juggernaut":
         if (card.code == "40121b") return true;
         else return false;
       case "mister_sinister":
-        if (card.imagesrc != undefined && card.threat) {
-          if (card.code == "11007") return true;
-          else return false;
+        if (card.code == "40139b") {
+          card.threat = '-';
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2023/08/40139b.jpg';
+          return true;
         }
+        if (card.code == "40140b") { //Phase 2B Flight
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2023/08/40140b.jpg';
+          return true;
+        }
+        if (card.code == "40141b") { //Phase 2B Super Strength
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2023/08/40141b.jpg';
+          return true;
+        }
+        if (card.code == "40142b") { //Phase 2B Telepathy
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2023/08/40142b.jpg';
+          return true;
+        }
+        // if (card.code == "40143b") { // Phase 3B
+        //   card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2023/08/40143b.jpg';
+        //   return true;
+        // }
         else return false;
       case "stryfe":
         if (card.code == "40166b") return true;
-        if (card.code == "40167b") return true;
+        // if (card.code == "40167b") return true;
         else return false;
       case "unus":
         if (card.code == "45062b") return true;
@@ -259,6 +328,12 @@ export class GameOptionsComponent implements OnInit {
         if (card.code == "45085b") return true;
         else return false;
       case "apocalypse":
+
+        card.threat = this.rawEncounterCards.find((c: any) => c.code == "45101a").health * this.playersQty; //Easy
+        // card.threat = this.rawEncounterCards.find((c: any) => c.code == "45101b").health * this.playersQty;
+        // card.threat = this.rawEncounterCards.find((c: any) => c.code == "45102a").health * this.playersQty;
+        // card.threat = this.rawEncounterCards.find((c: any) => c.code == "45102b").health * this.playersQty;
+
         if (card.code == "45103b") return true;
         else return false;
       case "dark_beast":
@@ -266,41 +341,60 @@ export class GameOptionsComponent implements OnInit {
         else return false;
       case "en_sabah_nur":
         if (card.code == "45147b") return true;
-        if (card.code == "45148b") return true;
+        // if (card.code == "45148b") return true;
         else return false;
       case "black_widow_villain":
-        if (card.imagesrc != undefined && card.threat) {
-          if (card.code == "50067b") return true;
-          else return false;
+
+        card.escalation_threat = 1 * this.playersQty; //Easy
+        // card.escalation_threat = 2 * this.playersQty;
+        // card.escalation_threat = 3 * this.playersQty;
+
+        if (card.code == "50067b") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2025/03/50067b.jpg';
+          return true;
         }
         else return false;
       case "batroc":
-        if (card.imagesrc != undefined && card.threat) {
-          if (card.code == "50087b") return true;
-          if (card.code == "50088b") return true;
-          if (card.code == "50089b") return true;
-          else return false;
+        if (card.code == "50087b") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2025/03/50087b.jpg';
+          return true;
         }
+        // if (card.code == "50088b") {
+        //   card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2025/03/50088b.jpg';
+        //   return true;
+        // }
+        // if (card.code == "50089b") {
+        //   card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2025/03/50089b.jpg';
+        //   return true;
+        // }
         else return false;
       case "m.o.d.o.k.":
-        if (card.imagesrc != undefined && card.threat) {
-          if (card.code == "50104b") return true;
-          else return false;
+        if (card.code == "50104b") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2025/03/50104b.jpg';
+          return true;
         }
         else return false;
       case "thunderbolts":
-        if (card.imagesrc != undefined && card.threat) {
-          if (card.code == "50130b") return true;
-          else return false;
+        if (card.code == "50130b") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2025/03/50130b.jpg';
+          return true;
         }
         else return false;
       case "baron_zemo":
-        if (card.imagesrc != undefined && card.threat) {
-          if (card.code == "50167b") return true;
-          if (card.code == "50168b") return true;
-          if (card.code == "50169b") return true;
-          else return false;
+
+        if (card.code == "50167b") {
+          card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2025/03/50167b.jpg';
+          return true;
         }
+        // if (card.code == "50168b") {
+        //   card.threat = "-";
+        //   card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2025/03/50168b.jpg';
+        //   return true;
+        // }
+        // if (card.code == "50169b") {
+        //   card.imagesrc = 'https://hallofheroeslcg.com/wp-content/uploads/2025/03/50169b.jpg';
+        //   return true;
+        // }
         else return false;
       default:
         return false;
